1e5e00bd297b642baa8c5e5c505490d2c503dfc2
rbhein <hein.rafael@hotmail.com>
2025-10-29 20:40:47 -0300

    Server - Ajuste porta 2

--- front/cambio.js
+++ front/cambio.js
@@ -19,7 +19,7 @@ function formatNumber(n, digits = 4) {
 // ======== BUSCA VIA BACKEND ========
 async function fetchRates() {
   try {
-    const res = await fetch(`/api/rates?nocache=${Date.now()}`);
+    const res = await fetch(`https://solutio.up.railway.app/api/rates?nocache=${Date.now()}`);
     const data = await res.json();
 
     const usdPrice = data?.USD;
